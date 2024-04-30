7ef9cca69f4ab93959f57a59fec4ba068dfe5b82
Diego <diazdiad@oregonstate.edu>
2024-04-30 15:19:43 -0700

    saying 'no' to replacing an already existing recipe now takes the user back to the main menu

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def main():
     search_recipe_output_file = "search_recipe_output.txt"
 
     while True:
+        restart = False
         print("\nPress ENTER to see contents of the recipe book")
         print("1 to add a recipe")
         print("2 to remove a recipe (cannot be undone)")
@@ -30,42 +31,29 @@ def main():
                     print()
 
         elif user_input == "1":
-
             while True:
-
                 recipe_name = input("Enter the name of the recipe (or 'back' to go back): ")
-
                 if recipe_name.lower() == 'back':
                     break
-
                 with open(recipe_file, 'r') as file:
-
                     reader = csv.reader(file, delimiter=';')
-
                     for row in reader:
-
                         if row[0] == recipe_name:
-
                             replace = input("This recipe already exists. Do you want to replace it? (yes/no): ")
-
                             if replace.lower() != 'yes':
-                                continue
-
-                ingredients = input("Enter the ingredients of the recipe separated by commas (or 'back' to go back): ")
-
-                if ingredients.lower() == 'back':
-                    continue
-
-                instructions = input("Enter the instructions of the recipe separated by commas (or 'back' to go back): ")
-
-                if instructions.lower() == 'back':
-                    continue
-
-                with open(add_recipe_input_file, 'w') as file:
-
-                    file.write(f'{recipe_name};{ingredients};{instructions}')
-
-                break
+                                restart = True
+                                break
+                    if restart:
+                        break
+                    ingredients = input("Enter the ingredients of the recipe separated by commas (or 'back' to go back): ")
+                    if ingredients.lower() == 'back':
+                        continue
+                    instructions = input("Enter the instructions of the recipe separated by commas (or 'back' to go back): ")
+                    if instructions.lower() == 'back':
+                        continue
+                    with open(add_recipe_input_file, 'w') as file:
+                        file.write(f'{recipe_name};{ingredients};{instructions}')
+                    break
 
         elif user_input == "2":
             recipe_name = input("Enter the name of the recipe to remove: ")
@@ -76,7 +64,7 @@ def main():
             recipe_name = input("Enter the name of the recipe to search: ")
             with open(search_recipe_input_file, 'w') as file:
                 file.write(recipe_name)
-            time.sleep(3)
+            time.sleep(2)
             with open(search_recipe_output_file, 'r') as file:
                 print(file.read())
 
@@ -86,6 +74,8 @@ def main():
         else:
             print("Invalid input. Please try again.")
 
+        if restart:
+            continue
 
 if __name__ == "__main__":
     main()
