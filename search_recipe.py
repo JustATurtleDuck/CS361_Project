ebf5609ade2ec1afcf1d43fd82b6efbf2392674b
Diego <diazdiad@oregonstate.edu>
2024-04-30 15:20:00 -0700

    Actually works now!

--- search_recipe.py
+++ search_recipe.py
@@ -9,11 +9,16 @@ while True:
         recipe_name = file.readline().strip()
         if recipe_name:
             with open(recipe_file, 'r') as recipe_file_open:
-                reader = csv.reader(recipe_file_open)
+                reader = csv.reader(recipe_file_open, delimiter=';')
                 for row in reader:
                     if row[0] == recipe_name:
                         with open(output_file, 'w') as output_file_open:
-                            output_file_open.write(f'Recipe: {row[0]}\nIngredients:\n' + '\n'.join(row[1:]))
+                            output_file_open.write(f'Recipe: {row[0]}\nIngredients:')
+                            for ingredient in row[1].strip('[]').split(','):
+                                output_file_open.write(f'\n- {ingredient.strip()}')
+                            output_file_open.write('\nInstructions:')
+                            for instruction in row[2].strip('[]').split(','):
+                                output_file_open.write(f'\n- {instruction.strip()}')
                         break
                 else:
                     with open(output_file, 'w') as output_file_open:
